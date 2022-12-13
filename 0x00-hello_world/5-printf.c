3426bb1dedf8c2c233a0a0e92e69051e486b6409
AndrewOndoma <117834170+AndrewOndoma@users.noreply.github.com>
2022-12-12 20:44:21 -0400

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,10 @@
 /**
  * main - Entry point
  * 
- * Description: 'the program's description'
- * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-   		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+   	return (0);
 }
