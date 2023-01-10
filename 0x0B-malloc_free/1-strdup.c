6e1d1079300b8944e401bdfed4d1538f521329af
AndrewOndoma <117834170+AndrewOndoma@users.noreply.github.com>
2023-01-10 23:33:15 +0100

    Completed all tasks for Malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - duplicates string
+ * @str: string to copy
+ * Return: char to newly duplicated string
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int j = 0;
+	char *allocated;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[j] != '\0')
+		j++;
+
+	allocated = malloc(sizeof(char) * j + 1);
+
+	if (allocated == NULL)
+		return (NULL);
+
+	for (i = 0; i < j; i++)
+		allocated[i] = str[i];
+
+	return (allocated);
+
+}
